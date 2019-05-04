e4b1f2439dd23549aee63e2c4c7bd0b29e325055
messaooudi <messaoudi.oussama.in@gmail.com>
2019-05-04 16:47:05 +0200

    double quote test fixe

--- mdx-lexer/src/symblos/__tests__/DOUBLE_QUOTE.test.ts
+++ mdx-lexer/src/symblos/__tests__/DOUBLE_QUOTE.test.ts
@@ -1,6 +1,6 @@
 import DOUBLE_QUOTE from "../DOUBLE_QUOTE";
 
 test('DOUBLE_QUOTE symbol', () => {
-  expect("").toMatch(DOUBLE_QUOTE.PATTERN as RegExp);
+  expect('"').toMatch(DOUBLE_QUOTE.PATTERN as RegExp);
   expect('something else').not.toMatch(DOUBLE_QUOTE.PATTERN as RegExp);
 });
